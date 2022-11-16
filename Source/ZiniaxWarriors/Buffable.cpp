0b3ad13f2137b80b95a12a76cb3904e7689f97eb
Gabriel Machado Marins <nufv12@gmail.com>
2022-11-16 15:49:13 +0000

    BuffFristTry

---
+++ Source/ZiniaxWarriors/Buffable.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Buffable.h"
+
+// Add default functionality here for any IBuffable functions that are not pure virtual.
